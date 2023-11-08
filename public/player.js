41c7438df60754dfaa72143e879021771ecb781e
danielSbastos <daniel.bastos1710@gmail.com>
2023-11-08 10:27:24 -0300

    Fix lints

--- public/player.js
+++ public/player.js
@@ -102,37 +102,3 @@ Soundfont.instrument(ac, urlInstrument['trompete']).then((instrument) => {
 Soundfont.instrument(ac, urlInstrument['tuba']).then((instrument) => {
   instrumentObj['tuba'] = instrument
 })
-
-// Soundfont.instrument(ac, 'https://raw.githubusercontent.com/gleitz/midi-js-soundfonts/gh-pages/MusyngKite/xylophone-mp3.js').then(function (instrument) {
-//   playSong = async function(instrument, url) {
-//     if (Player) Player.stop()
-
-//     const file = await getFileFromUrl(url, 'example-midi');
-
-//     var reader = new FileReader();
-//     if (file) reader.readAsArrayBuffer(file);
-
-//     reader.addEventListener('load', function () {
-//       Player = new MidiPlayer.Player(function(event) {
-//         if (event.name == 'Note on') {
-//           instrument.play(event.noteName, ac.currentTime, {gain:event.velocity/100});
-//         }
-
-//         document.getElementById('song-time').innerHTML =  formatTime(Player.getSongTime() - Player.getSongTimeRemaining());
-//         document.getElementsByClassName('btn-close')[0].addEventListener('click', function() {
-//           document.getElementById('play-bar-container').style.visibility = 'hidden';
-//           Player.stop();
-//         })
-//       });
-
-//       Player.on('playing', (currentTick) => {
-//         document.getElementById('play-track').value = 100 - Player.getSongPercentRemaining()
-//       });
-
-//       document.getElementById('play-bar-container').style.visibility = 'visible';
-//       document.getElementById('play-track').max = Player.getSongTime();
-//       Player.loadArrayBuffer(reader.result);
-//       Player.play()
-//     }, false);
-//   }
-// });
