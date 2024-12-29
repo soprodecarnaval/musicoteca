6285d52417125f3e2d281be684d590948c311649
Gustavo Brunoro <git@hitnail.net>
2024-12-29 13:31:05 -0300

    feat: collapse media generation steps

--- scripts/generateMsczAssets.sh
+++ scripts/generateMsczAssets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-MSCORE="/Applications/MuseScore 3.app/Contents/MacOS/mscore"
-BASE_REF="6451d5d"
+BASE_REF=$1
+MSCORE=$2
 MEDIA_GENERATION_JSON="/tmp/media-generation.json"
 git diff --name-only $BASE_REF HEAD | grep ".mscz" | jq -R -s 'split("\n") | map(select(.!="") | sub(".mscz";"") | {"in":"\(.).mscz", "out": [[ "\(.)_",".svg"],[ "\(.)_",".midi"],"\(.).midi","\(.).metajson"]})' > $MEDIA_GENERATION_JSON
 cat "$MEDIA_GENERATION_JSON" | jq
