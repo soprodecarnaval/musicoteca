b0700768889b82fda7c210ad7a0a8b568c173d3d
Gustavo Brunoro <git@hitnail.net>
2025-01-04 18:10:45 -0300

    fix: fetch main

--- scripts/generateMsczAssets.sh
+++ scripts/generateMsczAssets.sh
@@ -2,6 +2,6 @@
 BASE_REF=$1
 MSCORE=$2
 MEDIA_GENERATION_JSON="/tmp/media-generation.json"
-git diff --name-only --diff-filter=AM $BASE_REF HEAD | grep ".mscz" | jq -R -s 'split("\n") | map(select(.!="") | sub(".mscz";"") | {"in":"\(.).mscz", "out": [[ "\(.)_",".svg"],[ "\(.)_",".midi"],"\(.).midi","\(.).metajson"]})' > $MEDIA_GENERATION_JSON
+git diff --name-only --diff-filter=AM $BASE_REF HEAD | grep ".mscz\$" | jq -R -s 'split("\n") | map(select(.!="") | sub(".mscz";"") | {"in":"\(.).mscz", "out": [[ "\(.)_",".svg"],[ "\(.)_",".midi"],"\(.).midi","\(.).metajson"]})' > $MEDIA_GENERATION_JSON
 cat "$MEDIA_GENERATION_JSON" | jq
 $MSCORE -j "$MEDIA_GENERATION_JSON"
